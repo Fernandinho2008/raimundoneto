7dc28adf3c03c6dd1f84844b0df7a3f243857356
Fernandinho2008 <160048300+Fernandinho2008@users.noreply.github.com>
2024-09-18 20:02:20 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    ' Quem foi a primeira pessoa a viajar no Espaço? ',
+    ' Yuri Gagarin '
 )
 
 criaCartao(
@@ -18,6 +18,42 @@ criaCartao(
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qual a montanha mais alta do mundo? ',
+    '  Monte Everest. '
+)
+
+criaCartao(
+    'Programação',
+    'O que é mais pesado: 1 quilo de algodão ou 1 quilo de ferro?',
+    'Ambos têm o mesmo peso'
+)
+
+criaCartao(
+    'Geografia',
+    'Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
+    ' Aproximadamente 24 horas'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'A que temperatura a água ferve?',
+    '100 ºC.'
+)
+
+criaCartao(
+    'Programação',
+    'qual o alien que mais apareceu na serie ben 10',
+    'enormossauro'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'qual a akuma no mi que o luffy comeu',
+    'gomo gomo no mi modelo nika'
 )
